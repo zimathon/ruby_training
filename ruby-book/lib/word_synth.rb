5f80b95aca74bd95700b75e9dd18729a717ec3cb
Yusuke Sasajima <zimathon@gmail.com>
2019-05-07 23:05:48 +0900

    read #10 proc

--- ruby-book/lib/word_synth.rb
+++ ruby-book/lib/word_synth.rb
@@ -1,7 +1,15 @@
 class WordSynth
+  def initialize
+    @effects = []
+  end
+
+  def add_effect(effect)
+    @effects << effect
+  end
+
   def play(original_words)
-    ->(words) do
-      words.chars.map{ |c| c == '' ? c : c * rare}
+    @effects.inject(original_words) do |words, effect|
+      effect.call(words)
     end
   end
 end
